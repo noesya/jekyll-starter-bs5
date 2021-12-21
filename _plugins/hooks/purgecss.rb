e5107361b36856490783ebf7a08d189233209ffb
alexisben <alexiben7@gmail.com>
2021-12-21 15:01:40 +0100

    Fix purgecss

--- _plugins/hooks/purgecss.rb
+++ _plugins/hooks/purgecss.rb
@@ -1,3 +1,3 @@
 Jekyll::Hooks.register(:site, :post_write) do |_site|
-    system("purgecss --config ./purgecss.config.js --output _site/assets/css")
+    system("./node_modules/.bin/purgecss --config ./purgecss.config.js --output _site/assets/css")
 end
